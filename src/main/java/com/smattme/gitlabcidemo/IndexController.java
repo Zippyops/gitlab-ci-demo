b1c7bd3a0864194132fc1e8550baccb0d1f85f91
Seun Matt <smatt382@gail.com>
2021-09-02 11:33:32 +0100

    added gitlab ci

---
+++ src/main/java/com/smattme/gitlabcidemo/IndexController.java
@@ -0,0 +1,18 @@
+package com.smattme.gitlabcidemo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello World " + LocalDateTime.now();
+	}
+
+}
